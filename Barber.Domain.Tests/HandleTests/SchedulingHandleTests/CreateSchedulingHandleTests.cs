f6162ce12b35678f3de3ab12b06a6c4e9b31bf71
JoaoProfeta <amvjoao@hotmail.com>
2024-11-20 21:17:21 -0300

    Iniciando os testes do SchedulingHandle, Testes basicoos de : UpdateScheduing,CreateScheduling,DeleteScheduling. No mais tambem foi formatado algumas coisas, e foi adicionado as funcionalidades do FakeSchedulingRepository.

---
+++ Barber.Domain.Tests/HandleTests/SchedulingHandleTests/CreateSchedulingHandleTests.cs
@@ -0,0 +1,42 @@
+﻿using Barber.Domain.Command;
+using Barber.Domain.Command.Request.SchedulingRequests;
+using Barber.Domain.Entity;
+using Barber.Domain.Handler.SchedulingHandle;
+using Barber.Domain.Tests.FakeRepository;
+
+namespace Barber.Domain.Tests.HandleTests.SchedulingHandleTests;
+
+[TestClass]
+public class CreateSchedulingHandleTests
+{
+    private readonly CreateSchedulingCommandRequest _InvalidCommand = new CreateSchedulingCommandRequest(
+        SchedulingTime: DateTime.Now,
+        ProfessionalSelectedId: Guid.NewGuid(),
+        SchedulingStatus: Enum.ESchedulingStatus.Pending,
+        ServicesSelected: new List<Service>()
+        );
+    private readonly CreateSchedulingCommandRequest _ValidCommand = new CreateSchedulingCommandRequest(
+        SchedulingTime: DateTime.Now,
+        ProfessionalSelectedId: Guid.NewGuid(),
+        SchedulingStatus: Enum.ESchedulingStatus.Pending,
+        ServicesSelected: new List<Service> { new Service(name: "sobrancelha", status: Enum.EAvailabilityStatus.Unavailable) }
+        );
+    private readonly CreateSchedulingHandler _Handler = new CreateSchedulingHandler(new FakeSchedulingRepository());
+    private GenericCommandResult _result = new GenericCommandResult();
+
+    [TestMethod]
+    public async Task Create_Scheduling_Handle_Test_fail()
+    {
+        var result = await _Handler.Handle(_InvalidCommand);
+        _result = (GenericCommandResult)result;
+        Assert.AreEqual(_result.Success, false);
+    }
+    [TestMethod]
+    public async Task Create_Scheduling_Handle_Test_success()
+    {
+        var result = await _Handler.Handle(_ValidCommand);
+        _result = (GenericCommandResult)result;
+        Assert.AreEqual(_result.Success, true);
+    }
+}
+
---
+++ Barber.Domain.Tests/HandleTests/SchedulingHandleTests/DeleteSchedulingHandleTest.cs
@@ -0,0 +1,52 @@
+﻿using Barber.Domain.Command;
+using Barber.Domain.Command.Request.SchedulingRequests;
+using Barber.Domain.Entity;
+using Barber.Domain.Handler.SchedulingHandle;
+using Barber.Domain.Tests.FakeRepository;
+
+namespace Barber.Domain.Tests.HandleTests.SchedulingHandleTests;
+
+[TestClass]
+public class DeleteSchedulingHandleTest
+{
+    private readonly DeleteSchedulingCommandRequest _InvalidCommand;
+    private readonly DeleteSchedulingCommandRequest _ValidCommand;
+    private readonly FakeSchedulingRepository _repository;
+    private readonly DeleteSchedulingHandle _handle;
+    private GenericCommandResult _result = new GenericCommandResult();
+
+    public DeleteSchedulingHandleTest()
+    {
+        _repository = new FakeSchedulingRepository();
+        _handle = new DeleteSchedulingHandle(_repository);
+        Scheduling create = new Scheduling(
+                schedulingTime: DateTime.Now,
+                Guid.NewGuid(),
+                status: Enum.ESchedulingStatus.Accepted,
+                servicesSelected: new List<Service> { new Service(name: "sobrancelha", status: Enum.EAvailabilityStatus.Unavailable) }
+                );
+        _InvalidCommand = new DeleteSchedulingCommandRequest(Id: Guid.NewGuid());
+        _ValidCommand = new DeleteSchedulingCommandRequest(Id: create.Id);
+        _repository.CreateAsync(create).Wait();
+        
+
+    }
+
+    [TestMethod]
+    public async Task Delete_Scheduling_Handle_Test_Fail()
+    {
+        var result = await _handle.Handle(_InvalidCommand);
+        _result = (GenericCommandResult)result;
+        Assert.AreEqual(_result.Success, false);
+    }
+
+    [TestMethod]
+    public async Task Delete_Scheduling_Handle_Test_Success()
+    {
+        var result = await _handle.Handle(_ValidCommand);
+        _result = (GenericCommandResult)result;
+        Assert.AreEqual(_result.Success, true);
+    }
+
+}
+
