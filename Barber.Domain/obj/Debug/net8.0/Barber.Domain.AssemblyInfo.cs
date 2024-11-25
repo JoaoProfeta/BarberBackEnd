16bbbe217dfe809ba666a17b64c9eb3454530409
JoaoProfeta <amvjoao@hotmail.com>
2024-11-24 21:43:59 -0300

    tudo certo

--- Barber.Domain/obj/Debug/net8.0/Barber.Domain.AssemblyInfo.cs
+++ Barber.Domain/obj/Debug/net8.0/Barber.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barber.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8723d5d234114691ac0b56168ba22aa7f8534bf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d57cf9b3b940d75c69b76d288c51383832153b03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barber.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barber.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
